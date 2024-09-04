d09fe53f4b9852d5db274e87a37806446fed9df2
Zaheerkhn <zaheerkhan00457@gmail.com>
2024-09-04 18:29:34 +0530

    Added New Project

---
+++ Jarvis-PY/musicLibrary.py
@@ -0,0 +1,12 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21",
+    "atlantis" : "https://youtu.be/mR6l_TV8hS4?si=YeotgKnrWT6gYEsb",
+    "judas" : "https://youtu.be/OTmJmteT7hw?si=DvXSx2YBcWbV5eNN",
+    "bad" : "https://youtu.be/bU2EvRBUmxc?si=qRPNetTESgTCRzxh",
+    "sunsetz" : "https://youtu.be/5-rbSNzU_b8?si=J7b13zB9d3gnKzuE",
+    "beginning" : "https://youtu.be/B3Z4XGAxJB0?si=5NrPfwRy1ywDq3Vc"
+
+}
